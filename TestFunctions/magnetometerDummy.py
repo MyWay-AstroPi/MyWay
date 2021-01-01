7cbb46144244e86e9699c28e83874e6275c29000
Andrea-Tomatis <58698567+Andrea-Tomatis@users.noreply.github.com>
2021-01-01 14:52:29 +0100

    Update magnetometerDummy.py

--- TestFunctions/magnetometerDummy.py
+++ TestFunctions/magnetometerDummy.py
@@ -16,5 +16,8 @@ def get_compass_raw_Dummy():
         
 magnetometer_values = get_compass_raw_Dummy()
 while True:
+    try:
 	mag_x, mag_y, mag_z = next(magnetometer_values) 
 	data_logger.info('%s , %s , %s', round(mag_x, 4), round(mag_y, 4), round(mag_z, 4))
+    except StopIteration:
+	break
