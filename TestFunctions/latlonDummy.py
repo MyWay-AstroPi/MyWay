dc01520aea7b8feb4ff6bc30693797b1384234f7
Andrea-Tomatis <58698567+Andrea-Tomatis@users.noreply.github.com>
2021-01-01 14:27:05 +0100

    Update latlonDummy.py

--- TestFunctions/latlonDummy.py
+++ TestFunctions/latlonDummy.py
@@ -12,10 +12,10 @@ def get_latlon_Dummy():
         
         lat_value = [float(i) for i in str(sublat).split(':')]
         long_value = [float(i) for i in str(sublong).split(':')]
-        yield  lat_str, long_str
+        yield  lat_value, long_value
         
         
 cords = get_latlon_Dummy()
 while True:
 	lat, lon = next(cords)
-	data_logger.info('%f %f',lat,lon)
+	data_logger.info('%f %f %f %f %f %f',lat[0],lat[1],lat[2],lon[0],lon[1],lon[2])
